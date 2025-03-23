dfa5c92e13f129e970376a674bff9de18c632c3f
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-03-23 04:14:33 +0000

    Update settings.py

--- baol_distributions/settings.py
+++ baol_distributions/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-xxaoowwd6x8s1h1)zc$!t@gf$=)))nu#g!3j-4c&y^28zlf7nl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["baol-distributions.up.railway.app", "127.0.0.1", "localhost"]
-CSRF_TRUSTED_ORIGINS = ["https://baol-distributions.up.railway.app"]
+ALLOWED_HOSTS = ['baol-distributions.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://baol-distributions.up.railway.app']
 
 
 
