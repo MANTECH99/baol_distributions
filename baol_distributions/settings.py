5887eaa9a220c52efb8086ed43249db5243a84be
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-03-23 02:48:17 +0000

    Update settings.py

--- baol_distributions/settings.py
+++ baol_distributions/settings.py
@@ -80,10 +80,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'baol_distributions.wsgi.application'
 
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default='mysql://root:BztDqOOnNGjXZQrQbprcGWopYraiarKz@mysql.railway.internal:3306/railway', conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
