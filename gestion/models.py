51924e7b5f79482a6c22fcc7adae22e2fbd1c51d
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-03-25 14:08:36 +0000

    Update models.py

--- gestion/models.py
+++ gestion/models.py
@@ -16,7 +16,7 @@ class StatutCamion(models.Model):
         ("En attente", "En attente"),
         ("En panne", "En panne"),
         ("Travaille pas", "Travaille pas"),
-    ])
+    ]
 
     statut = models.CharField(max_length=20, choices=STATUT_CHOICES, default='attente')
 
