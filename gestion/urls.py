e40953ec7f7f8e86c380e1702ea770ae3fdcdc70
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-03-31 19:57:31 +0000

    Update urls.py

--- gestion/urls.py
+++ gestion/urls.py
@@ -2,7 +2,8 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
-from .views import liste_livraisons, exporter_livraisons_excel, register, user_login
+from .views import liste_livraisons, exporter_livraisons_excel, register, user_login, liste_rapports, \
+    telecharger_rapport, ajouter_rapport, supprimer_rapport
 from .views import ajouter_livraison
 from .views import modifier_livraison, supprimer_livraison
 from .views import dashboard
@@ -19,6 +20,10 @@ path('ajouter/', ajouter_livraison, name='ajouter_livraison'),
     path('exporter-livraisons-excel/', exporter_livraisons_excel, name='exporter_livraisons_excel'),
     path('login/', user_login, name='login'),
     path('register/', register, name='register'),
+        path('rapports/', liste_rapports, name='liste_rapports'),
+    path('rapports/telecharger/<int:rapport_id>/', telecharger_rapport, name='telecharger_rapport'),
+    path('rapports/ajouter/', ajouter_rapport, name='ajouter_rapport'),
+    path('supprimer_rapport/<int:rapport_id>/', supprimer_rapport, name='supprimer_rapport'),
 
 
 ]
